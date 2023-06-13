68047d1f3775d42fd8519ac5b105bea108b3d3ff
SantinoCappello <scappello8@gmail.com>
2023-06-13 12:59:58 -0300

    generos

---
+++ programacion1-main/js/detalle-genero.js
@@ -0,0 +1,58 @@
+let qs = location.search; 
+let qstoObjetoLiteral = new URLSearchParams(qs); 
+let id = qstoObjetoLiteral.get("id"); 
+let seccionCantantes = document.querySelector(".generos");
+
+
+const url = `https://cors-anywhere.herokuapp.com/https://api.deezer.com/genre/${id}`
+const url2 = `https://cors-anywhere.herokuapp.com/https://api.deezer.com/genre/${id}/artists`
+
+
+fetch(url)
+  .then(function (response) {
+    
+    return response.json(); 
+  })
+  .then(function (data) {
+    
+    console.log(data);
+
+    
+    let title = document.querySelector("h4");
+    let image = document.querySelector(".imagen");
+
+    
+
+    title.innerText = `El genero es: ${data.name}`; 
+    image.src = data.picture;
+  })
+
+  .catch(function (errores) {
+    console.log(errores);
+  });
+
+fetch(url2)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    let generoArtista = data.data;
+    console.log(generoArtista);
+
+    for (let i = 0; i < 5; i++) {
+      
+
+      seccionCantantes.innerHTML += `<article class="Albumes"> 
+      <a href="./album.html?id=${generoArtista[i].id}">
+        <img class="imagen" src="${generoArtista[i].picture_medium}" alt="">  
+      </a>
+     <a class="url1" href="./album.html?id=${generoArtista[i].id}"> <h4>${generoArtista[i].name}</h4></a> 
+  </article>`;
+    }
+  })
+
+  .catch(function (errores) {
+    console.log(errores);
+  });
+
+
---
+++ programacion1-main/js/generos.js
@@ -0,0 +1,23 @@
+let url = 'https://cors-anywhere.herokuapp.com/https://api.deezer.com/genre';
+
+for (let i = 1; i < 13; i++){
+fetch(url)
+    .then(function(response){
+    return response.json();
+    })
+    .then(function(datos){
+        console.log(datos)
+        let generos = datos.data;
+        let contenedor = document.querySelector('.ListaGeneros');
+        
+        contenedor.innerHTML += 
+        `<article class="articlecanc">
+        <img src="${generos[i].picture_medium}" alt="" class="fotodiscos">
+        <h4>${generos[i].name}</h4>
+        <a href="./detalle-genero.html?id=${generos[i].id}" class="verMas">Ver más</a>
+        </article>`
+        
+    })
+    .catch(function(error){
+        console.log("Error: " + error);
+    })}
